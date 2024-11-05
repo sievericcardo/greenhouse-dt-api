148fecff62508b65003cd554c84a175f02525c88
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-05 20:07:39 +0100

    Build file

---
+++ src/main/kotlin/org/smolang/greenhouse/api/config/SecurityConfig.kt
@@ -0,0 +1,45 @@
+package no.uio.org.smolang.greenhouse.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+open class SecurityConfig {
+    @Bean
+    open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { authz ->
+            authz
+                .requestMatchers(
+                    "/",
+                    "/api/**",
+                    "/api-docs/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/v*/api-docs",
+                    "/swagger-resources/**").permitAll()
+                .anyRequest().authenticated()
+        }
+
+        return http.build()
+    }
+
+    @Bean
+    open fun webSecurityCustomizer () : WebSecurityCustomizer {
+        return WebSecurityCustomizer { webSecurity ->
+            webSecurity.ignoring().requestMatchers(
+                "/websocket",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/v*/api-docs/**",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/api/**"
+            )
+        }
+    }
+}
