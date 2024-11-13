34a3ca5250423f81e96510e36ff5210bbb012ae4
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-13 16:43:06 +0100

    Added method to update pressure of pumps

--- src/main/kotlin/org/smolang/greenhouse/api/controller/PumpController.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/controller/PumpController.kt
@@ -6,13 +6,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import no.uio.microobject.runtime.REPL
 import org.apache.jena.query.QuerySolution
 import org.apache.jena.query.ResultSet
+import org.apache.jena.rdfconnection.RDFConnectionFactory
+import org.apache.jena.update.*
 import org.smolang.greenhouse.api.config.REPLConfig
 import org.smolang.greenhouse.api.model.Pump
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.logging.Logger
+import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
+
+
+data class PumpRequest (
+    val pumpGpioPin: Int,
+    val pumpId: String,
+    val waterPressure: Double
+)
 
 @RestController
 @RequestMapping("/api/pumps")
@@ -182,5 +190,62 @@ class PumpController (
         log.info("Pumps: $pumpsList")
 
         return ResponseEntity.ok(pumpsList)
+    }
+
+    @Operation(summary = "Update pressure to a pump")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Successfully updated the pump pressure"),
+        ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
+    ])
+    @PostMapping("/update")
+    fun updatePumpPressure(@SwaggerRequestBody(description = "Pump to be updated") @RequestBody pumpRequest: PumpRequest) : ResponseEntity<String> {
+        log.info("Updating pump pressure")
+
+        val tripleStore =System.getenv().getOrDefault("TRIPLESTORE_URL", "http://localhost:3030/ds")
+        val prefix = System.getenv().getOrDefault("BASE_PREFIX_URI", "http://www.smolang.org/greenhouseDT#")
+
+        val updatedPump = Pump(pumpRequest.pumpGpioPin, pumpRequest.pumpId, pumpRequest.waterPressure, PumpState.Unknown)
+
+        val updateQuery = """
+            PREFIX ast: <$prefix>
+            
+            DELETE {
+                ?pump ast:waterPressure ?oldWaterPressure .
+            }
+            INSERT {
+                ?pump ast:waterPressure ${updatedPump.waterPressure} .
+            }
+            WHERE {
+                ?pump a ast:Pump ;
+                    ast:pumpGpioPin ${updatedPump.pumpGpioPin} ;
+                    ast:pumpId "${updatedPump.pumpId}" ;
+                    ast:waterPressure ?oldWaterPressure .
+            }
+        """
+
+        val updateRequest: UpdateRequest = UpdateFactory.create(updateQuery)
+        val fusekiEndpoint = tripleStore + "/update"
+        val updateProcessor: UpdateProcessor = UpdateExecutionFactory.createRemote(updateRequest, fusekiEndpoint)
+
+        try {
+            updateProcessor.execute()
+        } catch (e: Exception) {
+            return ResponseEntity.badRequest().body("Error: the update query could not be executed.")
+        }
+
+        val repl: REPL = replConfig.repl()
+        repl.interpreter!!.tripleManager.regenerateTripleStoreModel()
+        repl.interpreter!!.evalCall(
+            repl.interpreter!!.getObjectNames("AssetModel").get(0),
+            "AssetModel",
+            "reconfigure")
+        repl.interpreter!!.evalCall(
+            repl.interpreter!!.getObjectNames("AssetModel").get(0),
+            "AssetModel",
+            "reclassify")
+
+        return ResponseEntity.ok("Pump pressure updated")
     }
 }
