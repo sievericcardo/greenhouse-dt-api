0a20abae94b65b21ad436905570d62f14391f167
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-20 17:50:15 +0100

    Demo mode in main

--- src/main/kotlin/org/smolang/greenhouse/api/controller/HomeController.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/controller/HomeController.kt
@@ -1,13 +1,21 @@
 package org.smolang.greenhouse.api.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.smolang.greenhouse.api.config.EnvironmentConfig
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.logging.Logger
 
 @RestController
 @RequestMapping("/api")
-class HomeController {
+class HomeController (
+    private val environment: EnvironmentConfig
+) {
 
     private val log : Logger = Logger.getLogger(HomeController::class.java.name)
 
@@ -15,5 +23,17 @@ class HomeController {
     fun status(): String {
         log.info("Application is running")
         return "Application is running"
+    }
+
+    @Operation(summary = "Set the application in demo mode")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Successfully set the application in demo mode")
+    ])
+    @PostMapping("/set-demo")
+    fun setDemoMode(): ResponseEntity<String> {
+        log.info("Setting the application in demo mode")
+        val currentDemoMode = environment.getOrDefault("DEMO", "false").toBoolean()
+        environment.set("DEMO", (!currentDemoMode).toString())
+        return ResponseEntity.ok("Application is now in ${(!currentDemoMode).toString()} mode")
     }
 }
