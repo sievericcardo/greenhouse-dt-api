d0b62898f6a35f4a99833919903cd1108a10bd3f
Riccardo Sieve <sievericcardo@gmail.com>
2024-12-31 14:40:59 +0100

    Added name and time in Model for Pump

--- src/main/kotlin/org/smolang/greenhouse/api/model/Pump.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/model/Pump.kt
@@ -5,6 +5,8 @@ import org.smolang.greenhouse.api.controller.PumpState
 class Pump (
     val pumpGpioPin: Int,
     val pumpId: String,
+    val modelName: String,
+    val lifeTime: Int,
     val temperature: Double,
     val pumpStatus: PumpState
 ) {
