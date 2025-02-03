b14c689b69594839f73cb36103450b12e6440538
Riccardo Sieve <sievericcardo@gmail.com>
2025-02-03 21:08:54 +0100

    Changed configuration to have it at startup

--- src/main/kotlin/org/smolang/greenhouse/api/config/QueueConfig.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/config/QueueConfig.kt
@@ -1,15 +1,22 @@
 package org.smolang.greenhouse.api.config
 
+import jakarta.annotation.PostConstruct
 import org.apache.activemq.ActiveMQConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class QueueConfig {
-    @Bean
-    open fun activeMQConnectionFactory(): ActiveMQConnectionFactory {
-        val activeMQConnectionFactory = ActiveMQConnectionFactory()
+
+    private lateinit var activeMQConnectionFactory: ActiveMQConnectionFactory
+
+    @PostConstruct
+    open fun initActiveMQ() {
         activeMQConnectionFactory.brokerURL = "tcp://" + System.getenv("ACTIVEMQ_HOST") + ":" + System.getenv("ACTIVEMQ_PORT")
+    }
+
+    @Bean
+    open fun getActiveMQConnectionFactory(): ActiveMQConnectionFactory {
         return activeMQConnectionFactory
     }
 }
