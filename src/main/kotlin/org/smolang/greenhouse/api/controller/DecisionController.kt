994d47b69fe993ba8c4d24b699417e92dca5a344
Riccardo Sieve <sievericcardo@gmail.com>
2025-02-03 21:09:28 +0100

    Created controller to run a decision every hour
    Note: might be changed to 6 hours or 24 hours

---
+++ src/main/kotlin/org/smolang/greenhouse/api/controller/DecisionController.kt
@@ -0,0 +1,24 @@
+package org.smolang.greenhouse.api.controller
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.smolang.greenhouse.api.service.DecisionService
+import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
+
+@RestController
+@RequestMapping("/decision")
+class DecisionController (
+    private val decisionService: DecisionService
+) {
+
+    @Scheduled(fixedRate = 3600000) // 3600000 milliseconds = 60 minutes
+    @Operation(summary = "Make a decision every 6 hours")
+    fun makeDecision() {
+        val plantsToWater = decisionService.execSmol()
+        decisionService.waterControl(plantsToWater)
+    }
+}
