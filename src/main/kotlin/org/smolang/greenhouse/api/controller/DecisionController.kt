1207f979718c086b81a9a4954bd2e874aa3a2b98
Riccardo Sieve <sievericcardo@gmail.com>
2025-03-13 17:24:06 +0100

    Setup tasks as a component

--- src/main/kotlin/org/smolang/greenhouse/api/controller/DecisionController.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/controller/DecisionController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.smolang.greenhouse.api.service.DecisionService
+import java.util.logging.Logger
 import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
 
 @RestController
@@ -15,10 +16,16 @@ class DecisionController (
     private val decisionService: DecisionService
 ) {
 
-    @Scheduled(fixedRate = 3600000) // 3600000 milliseconds = 60 minutes
+    private val log: Logger = Logger.getLogger(DecisionController::class.java.name)
+
+//    @Scheduled(fixedRate = 3600000) // 3600000 milliseconds = 60 minutes
+//    @Scheduled(fixedRate = 3) // 3600000 milliseconds = 60 minutes
+    @Scheduled(cron = "*/5 * * * * *") // Execute every 5 second
     @Operation(summary = "Make a decision every 6 hours")
     fun makeDecision() {
+        log.info( "Start decision process")
         val plantsToWater = decisionService.execSmol()
         decisionService.waterControl(plantsToWater)
+        log.info( "End decision process")
     }
 }
---
+++ src/main/kotlin/org/smolang/greenhouse/api/tasks/DecisionTasks.kt
@@ -0,0 +1,26 @@
+package org.smolang.greenhouse.api.tasks
+
+import io.swagger.v3.oas.annotations.Operation
+import org.smolang.greenhouse.api.service.DecisionService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.logging.Logger
+
+@Component
+class DecisionTasks (
+    private val decisionService: DecisionService
+) {
+
+    private val log: Logger = Logger.getLogger(DecisionTasks::class.java.name)
+
+    //    @Scheduled(fixedRate = 3600000) // 3600000 milliseconds = 60 minutes
+//    @Scheduled(fixedRate = 3) // 3600000 milliseconds = 60 minutes
+    @Scheduled(cron = "*/5 * * * * *") // Execute every 5 second
+    @Operation(summary = "Make a decision every 6 hours")
+    fun makeDecision() {
+        log.info( "Start decision process")
+        val plantsToWater = decisionService.execSmol()
+        decisionService.waterControl(plantsToWater)
+        log.info( "End decision process")
+    }
+}
