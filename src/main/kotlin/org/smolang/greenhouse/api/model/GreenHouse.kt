4d1e686889451aa13ec8708534e394906178d6db
Riccardo Sieve <sievericcardo@gmail.com>
2025-07-20 16:01:45 +0200

    Add GreenHouse model class
    Introduces the GreenHouse data model with properties for greenhouseId, sections, waterBuckets, lightSensor, and temperatureHumiditySensor.

---
+++ src/main/kotlin/org/smolang/greenhouse/api/model/GreenHouse.kt
@@ -0,0 +1,9 @@
+package org.smolang.greenhouse.api.model
+
+class GreenHouse (
+    val greenhouseId: String,
+    val sections: List<Section>,
+    val waterBuckets: List<WaterBucket>,
+    val lightSensor: LightSensor,
+    val temperatureHumiditySensor: TemperatureHumiditySensor
+)
