5e2c32613735fa0af7962d57476ad5a8c46ca2cd
Riccardo Sieve <sievericcardo@gmail.com>
2025-07-20 15:40:43 +0200

    Add Actuator model class
    Introduces the Actuator class in the model package with a single actuatorId property and a custom toString implementation. This class implements Serializable for object serialization.

---
+++ src/main/kotlin/org/smolang/greenhouse/api/model/Actuator.kt
@@ -0,0 +1,11 @@
+package org.smolang.greenhouse.api.model
+
+import java.io.Serializable
+
+open class Actuator (
+    val actuatorId: String,
+) : Serializable {
+    override fun toString(): String {
+        return "Actuator(actuatorId='$actuatorId')"
+    }
+}
