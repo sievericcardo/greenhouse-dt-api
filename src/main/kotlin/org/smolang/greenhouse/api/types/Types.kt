f49c8a419b59edaec30dbc2cbc330e5d2787c873
Riccardo Sieve <sievericcardo@gmail.com>
2025-08-02 11:46:09 +0200

    Add and update greenhouse API request types
    Introduced new data classes for light switch, section, pot, and various sensor creation and update requests. Updated existing request types to include additional fields and removed unused fields to better reflect the API requirements.

--- src/main/kotlin/org/smolang/greenhouse/api/types/Types.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/types/Types.kt
@@ -30,7 +30,6 @@ data class CreatePumpRequest(
 )
 
 data class UpdatePumpRequest (
-    val actuatorId: String,
     val pumpChannel: Int? = null,
     val modelName: String? = null,
     val lifeTime: Int? = null,
@@ -41,20 +40,82 @@ data class DeletePumpRequest (
     val actuatorId: String
 )
 
+data class CreateLightSwitchRequest(
+    val actuatorId: String
+)
+
 data class CreateGreenHouseRequest (
-    val greenhouseId: String
+    val greenhouseId: String,
+    val sections: List<String>,
+    val waterBuckets: List<String>,
+    val lightSensor: String? = null,
+    val thSensor: String? = null,
 )
 
 data class CreateSectionRequest (
-    val sectionId: String
+    val sectionId: String,
+    val pots: List<String>
+)
+
+data class UpdateSectionRequest (
+    val pots: List<String>?
 )
 
 data class CreateWaterBucketRequest (
-    val bucketId: String,
-    val waterLevel: Double
+    val bucketId: String
 )
 
 data class UpdateWaterBucketRequest (
-    val bucketId: String,
-    val newWaterLevel: Double
+    val bucketId: String
+)
+
+data class CreatePotRequest (
+    val potId: String,
+    val plants: List<String>,
+    val moistureSensor: String? = null,
+    val nutrientSensor: String? = null,
+    val pump: String
+)
+
+data class UpdatePotRequest (
+    val plants: List<String>? = null,
+    val moistureSensor: String? = null,
+    val nutrientSensor: String? = null,
+    val pump: String? = null
+)
+
+data class CreateMoistureSensor (
+    val sensorId: String,
+    val sensorProperty: String,
+)
+
+data class UpdateMoistureSensor (
+    val sensorProperty: String?,
+)
+
+data class CreateNutrientSensor (
+    val sensorId: String,
+    val sensorProperty: String,
+)
+
+data class UpdateNutrientSensor (
+    val sensorProperty: String?,
+)
+
+data class CreateLightSensor (
+    val sensorId: String,
+    val sensorProperty: String,
+)
+
+data class UpdateLightSensor (
+    val sensorProperty: String?,
+)
+
+data class CreateTemperatureHumiditySensor (
+    val sensorId: String,
+    val sensorProperty: String,
+)
+
+data class UpdateTemperatureHumiditySensor (
+    val sensorProperty: String?,
 )
