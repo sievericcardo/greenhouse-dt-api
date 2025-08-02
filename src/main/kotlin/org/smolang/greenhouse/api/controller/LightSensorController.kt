d6c4f67cfdb766981cdd25bd712c90a5cf68c91d
Riccardo Sieve <sievericcardo@gmail.com>
2025-08-02 15:38:06 +0200

    Update LightSensorController.kt

--- src/main/kotlin/org/smolang/greenhouse/api/controller/LightSensorController.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/controller/LightSensorController.kt
@@ -1,6 +1,5 @@
 package org.smolang.greenhouse.api.controller
 
-
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
