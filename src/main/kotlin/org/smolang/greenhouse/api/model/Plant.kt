a4834d411155b14d2331f7e7656e3b29923e0fd5
Riccardo Sieve <sievericcardo@gmail.com>
2025-10-29 11:21:34 +0100

    Add moistureState to Plant model
    Introduced a new property, moistureState of type PlantMoistureState, to the Plant data model. This allows for more detailed tracking of a plant's moisture condition.

--- src/main/kotlin/org/smolang/greenhouse/api/model/Plant.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/model/Plant.kt
@@ -1,10 +1,13 @@
 package org.smolang.greenhouse.api.model
 
+import org.smolang.greenhouse.api.types.PlantMoistureState
+
 class Plant (
     val plantId: String,
     val familyName: String,
     val moisture: Double?,
     val healthState: String?,
-    val status: String?
+    val status: String?,
+    val moistureState: PlantMoistureState
 ) {
 }
