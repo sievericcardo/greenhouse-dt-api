6f3a44902e1a35c0e71110bb6d8636ab5d9fb44d
Riccardo Sieve <sievericcardo@gmail.com>
2025-10-30 10:52:48 +0100

    Changed order

--- src/main/kotlin/org/smolang/greenhouse/api/config/ComponentsCacheInitializer.kt
+++ src/main/kotlin/org/smolang/greenhouse/api/config/ComponentsCacheInitializer.kt
@@ -21,7 +21,8 @@ class ComponentsCacheInitializer(
     private val potService: PotService,
     private val pumpService: PumpService,
     private val sectionService: SectionService,
-    private val waterBucketService: WaterBucketService
+    private val waterBucketService: WaterBucketService,
+    private val componentsConfig: ComponentsConfig
 ) {
 
     private val log: Logger = LoggerFactory.getLogger(ComponentsCacheInitializer::class.java.name)
@@ -40,11 +41,11 @@ class ComponentsCacheInitializer(
             // Plants, pots, pumps, sections, water buckets
             waterBucketService.getAllWaterBuckets()
             pumpService.getAllPumps()
-            plantService.getAllPlants()
             potService.getPots()
+            plantService.getAllPlants()
             sectionService.getAllSections()
 
-            log.info("ComponentsCacheInitializer: caches populated (attempted)")
+            log.info("ComponentsCacheInitializer: caches populated (hopefully)")
         } catch (e: Exception) {
             log.warn("ComponentsCacheInitializer: error while populating caches: ${e.message}")
         }
